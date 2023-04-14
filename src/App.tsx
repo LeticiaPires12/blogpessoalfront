9617589a78cf8591e2faf8298b22062abada853f
Leticia <leticiapiress1@outlook.com>
2023-04-13 21:22:59 -0300

    Criando página login

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,30 @@
-import React from 'react';
-import Navbar from './components/estaticos/navbar/Navbar';
-import Footer from './components/estaticos/footer/Footer';
-import Home from './paginas/home/Home';
-import './App.css';
+import { useState } from 'react'
+import { BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom';
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Navbar from './components/estaticos/navbar/Navbar'
+import Home from './paginas/home/Home'
+import Login from './paginas/login/Login'
+import Footer from './components/estaticos/footer/Footer'
 
 function App() {
-  return (
-  <>
-    <Navbar />
-    <Home />
-    <Footer />
+  const [count, setCount] = useState(0)
 
-    </>
-  );
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <div style={{ minHeight: '80vh' }}>
+        <Routes> 
+          <Route path="/" element={<Login/>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </div>
+      <Footer />
+    </BrowserRouter>
+
+  )
 }
 
-export default App;
+export default App
--- src/components/estaticos/navbar/Navbar.tsx
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -1,164 +1,63 @@
+// Importar as dependências
 import React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
+import { Link } from 'react-router-dom';
+import Typography from '@material-ui/core/Typography';
+import Toolbar from '@material-ui/core/Toolbar';
+import AppBar from '@material-ui/core/AppBar';
+import Box from '@material-ui/core/Box';
+import './Navbar.css'
 
-const pages = ['início', 'temas', 'cadastrar tema'];
-const settings = ['conta', 'sair'];
+// Interface para as propriedades do componente
+interface NavbarProps {}
 
-function Navbar() {
-    const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
-    const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
-
-    const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-        setAnchorElNav(event.currentTarget);
-    };
-    const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-        setAnchorElUser(event.currentTarget);
-    };
-
-    const handleCloseNavMenu = () => {
-        setAnchorElNav(null);
-    };
-
-    const handleCloseUserMenu = () => {
-        setAnchorElUser(null);
-    };
-
-    return (
-        <>
-            <AppBar position="static">
-                <Container maxWidth="xl">
-                    <Toolbar disableGutters>
-                        <Typography
-                            variant="h6"
-                            noWrap
-                            component="a"
-                            href="/"
-                            sx={{
-                                mr: 2,
-                                display: { xs: 'none', md: 'flex' },
-                                fontFamily: 'monospace',
-                                fontWeight: 700,
-                                letterSpacing: '.3rem',
-                                color: 'inherit',
-                                textDecoration: 'none',
-                            }}
-                        >
-                            Blog Pessoal
-                        </Typography>
-
-                        <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-                            <IconButton
-                                size="large"
-                                aria-label="account of current user"
-                                aria-controls="menu-appbar"
-                                aria-haspopup="true"
-                                onClick={handleOpenNavMenu}
-                                color="inherit"
-                            >
-                                <MenuIcon />
-                            </IconButton>
-                            <Menu
-                                id="menu-appbar"
-                                anchorEl={anchorElNav}
-                                anchorOrigin={{
-                                    vertical: 'bottom',
-                                    horizontal: 'left',
-                                }}
-                                keepMounted
-                                transformOrigin={{
-                                    vertical: 'top',
-                                    horizontal: 'left',
-                                }}
-                                open={Boolean(anchorElNav)}
-                                onClose={handleCloseNavMenu}
-                                sx={{
-                                    display: { xs: 'block', md: 'none' },
-                                }}
-                            >
-                                {pages.map((page) => (
-                                    <MenuItem key={page} onClick={handleCloseNavMenu}>
-                                        <Typography textAlign="center">{page}</Typography>
-                                    </MenuItem>
-                                ))}
-                            </Menu>
-                        </Box>
-                        <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
-                        <Typography
-                            variant="h5"
-                            noWrap
-                            component="a"
-                            href=""
-                            sx={{
-                                mr: 2,
-                                display: { xs: 'flex', md: 'none' },
-                                flexGrow: 1,
-                                fontFamily: 'monospace',
-                                fontWeight: 700,
-                                letterSpacing: '.3rem',
-                                color: 'inherit',
-                                textDecoration: 'none',
-                            }}
-                        >
-                            LOGO
-                        </Typography>
-                        <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-                            {pages.map((page) => (
-                                <Button
-                                    key={page}
-                                    onClick={handleCloseNavMenu}
-                                    sx={{ my: 2, color: 'white', display: 'block' }}
-                                >
-                                    {page}
-                                </Button>
-                            ))}
-                        </Box>
-
-                        <Box sx={{ flexGrow: 0 }}>
-                            <Tooltip title="Open settings">
-                                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                                    <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-                                </IconButton>
-                            </Tooltip>
-                            <Menu
-                                sx={{ mt: '45px' }}
-                                id="menu-appbar"
-                                anchorEl={anchorElUser}
-                                anchorOrigin={{
-                                    vertical: 'top',
-                                    horizontal: 'right',
-                                }}
-                                keepMounted
-                                transformOrigin={{
-                                    vertical: 'top',
-                                    horizontal: 'right',
-                                }}
-                                open={Boolean(anchorElUser)}
-                                onClose={handleCloseUserMenu}
-                            >
-                                {settings.map((setting) => (
-                                    <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                                        <Typography textAlign="center">{setting}</Typography>
-                                    </MenuItem>
-                                ))}
-                            </Menu>
-                        </Box>
-                    </Toolbar>
-                </Container>
-            </AppBar>
-        </>
-    );
+// Função Navbar
+const Navbar: React.FC<NavbarProps> = () => {
+  return (
+    <AppBar position="static" style={{ backgroundColor: '#FF0084' }}>
+      <Toolbar>
+        <Box display="flex" alignItems="center" width="100%">
+          <Box mx={1}>
+            <Typography variant="h6" color="inherit">
+              <Link to="/home" style={{ textDecoration: 'none', color: 'inherit', cursor: 'pointer' }}>
+                Início
+              </Link>
+            </Typography>
+          </Box>
+          <Box mx={1}>
+            <Typography variant="h6" color="inherit">
+              <Link to="/tema" style={{ textDecoration: 'none', color: 'inherit', cursor: 'pointer' }}>
+                Tema
+              </Link>
+            </Typography>
+          </Box>
+          <Box mx={1}>
+            <Typography variant="h6" color="inherit">
+              <Link to="/cadastrar-tema" style={{ textDecoration: 'none', color: 'inherit', cursor: 'pointer' }}>
+                Cadastrar Temas
+              </Link>
+            </Typography>
+          </Box>
+          <Box marginLeft="auto" display="flex"> {/* Usar Box para alinhar os elementos à direita */}
+            <Link
+              to="/login"
+              style={{
+                textDecoration: 'none',
+                color: 'inherit',
+                marginLeft: '16px',
+                cursor: 'pointer', // Adicionar o estilo do cursor como pointer
+              }}
+            >
+              
+                <Typography variant="h6" color="inherit">
+                Logout
+              </Typography>
+              
+            </Link>
+          </Box>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
 }
 
-export default Navbar
+export default Navbar;
